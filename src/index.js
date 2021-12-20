ba900e23fa7ac9b56c33dd07bbfc45db2149b514
Oladipupo Adanikin <adanikindipo@gmail.com>
2021-12-20 19:50:43 +0100

    encrypt api keysss

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ import App from "./App";
 import "react-loading-skeleton/dist/skeleton.css";
 
 import FirebaseContext from "./context/firebase";
-import { FieldValue, firebase, firebaseConfig } from "./lib/firebase";
+import { FieldValue, firebase } from "./lib/firebase";
 
-console.log(firebaseConfig);
 ReactDOM.render(
   <FirebaseContext.Provider value={{ firebase, FieldValue }}>
     <App />
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,15 +4,15 @@ import "firebase/compat/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: "instagram-4de2d.firebaseapp.com",
-  projectId: "instagram-4de2d",
-  storageBucket: "instagram-4de2d.appspot.com",
-  messagingSenderId: "1004004729000",
-  appId: "1:1004004729000:web:c14ad0f2f9f0c08363229d",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
 };
 
 // Initialize Firebase
 const firebase = Firebase.initializeApp(firebaseConfig);
 const { FieldValue } = Firebase.firestore;
 
-export { firebase, FieldValue, firebaseConfig };
+export { firebase, FieldValue };
